e33389d4189ba24686110121b098ffe0cd4196d9
Daniel Markstedt <markstedt@gmail.com>
2024-01-12 09:10:28 +0900

    Create hosted download page

---
+++ download.php
@@ -0,0 +1,84 @@
+<?php
+ $title = "Download";
+ include_once "header.php";
+?>
+
+<div id="content">
+<h1>Download Netatalk</h1>
+
+<h3>Current Netatalk Releases</h3>
+<h5>Netatalk 3.x</h5>
+<ul>
+    <li>
+        <a href="https://sourceforge.net/projects/netatalk/files/netatalk-3-1-18/netatalk-3.1.18.tar.xz/download">
+            Download Netatalk 3.1.18 (xz compressed)
+        </a>
+    </li>
+    <li>
+        <a href="https://sourceforge.net/projects/netatalk/files/netatalk-3-1-18/netatalk-3.1.18.tar.bz2/download">
+            Download Netatalk 3.1.18 (bzip2 compressed)
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-3-1-18">
+            Release Notes
+        </a>
+    </li>
+</ul>
+<h5>Netatalk 2.x</h5>
+<ul>
+    <li>
+        <a href="https://sourceforge.net/projects/netatalk/files/netatalk-2-3-0/netatalk-2.3.0.tar.xz/download">
+            Download Netatalk 2.3.0 (xz compressed)
+        </a>
+    </li>
+    <li>
+        <a href="https://sourceforge.net/projects/netatalk/files/netatalk-2-3-0/netatalk-2.3.0.tar.bz2/download">
+            Download Netatalk 2.3.0 (bzip2 compressed)
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-2-3-0">
+            Release Notes
+        </a>
+    </li>
+</ul>
+
+<h3>Webmin Module</h3>
+<h5>Netatalk 3.x</h5>
+<ul>
+    <li>
+        <a href="https://sourceforge.net/projects/netatalk/files/Webmin/netatalk3-wbm-0.3.tgz/download">
+            Download Webmin Module 0.3
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk-webmin/releases/tag/netatalk3-0.3">
+            Release Notes
+        </a>
+    </li>
+</ul>
+<h5>Netatalk 2.x</h5>
+<ul>
+    <li>
+        <a href="https://sourceforge.net/projects/netatalk/files/Webmin/netatalk2-wbm-1.1.tgz/download">
+            Download Webmin Module 1.1
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk-webmin/releases/tag/netatalk2-1.1">
+            Release Notes
+        </a>
+    </li>
+</ul>
+
+<h3>AFP Test-Suite</h3>
+<ul>
+    <li>
+        <a href="https://sourceforge.net/projects/netatalk/files/Testsuite/afptest-1.0.6.tar.gz/download">
+            Download afptest 1.0.6
+        </a>
+    </li>
+</ul>
+
+<?php include_once "footer.php"; ?>
