7eec3ef93448e0de8328ee55fb7b445a9b9a1ca0
Daniel Markstedt <markstedt@gmail.com>
2024-01-14 23:02:24 +0900

    Add an index page for all published docs

---
+++ documentation.php
@@ -0,0 +1,370 @@
+<?php
+ $title = "Documentation";
+ include_once "header.php";
+?>
+
+<div id="content">
+<h1>Netatalk Documentation</h1>
+
+<h3>Current Netatalk Manuals</h3>
+<ul>
+    <li>
+        <a href="3.1/htmldocs">
+            Netatalk 3.1 manual
+        </a>
+    </li>
+    <li>
+        <a href="2.3/htmldocs">
+            Netatalk 2.3 manual
+        </a>
+    </li>
+</ul>
+<h3>Obsolete Netatalk Manuals</h3>
+<ul>
+    <li>
+        <a href="3.0/htmldocs">
+            Netatalk 3.0 manual
+        </a>
+    </li>
+    <li>
+        <a href="2.2/htmldocs">
+            Netatalk 2.2 manual
+        </a>
+    </li>
+    <li>
+        <a href="2.1/htmldocs">
+            Netatalk 2.1 manual
+        </a>
+    </li>
+    <li>
+        <a href="2.0/htmldocs">
+            Netatalk 2.0 manual
+        </a>
+    </li>
+</ul>
+
+<h3>Release Notes</h3>
+<ul>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-3-1-18">
+            Netatalk 3.1.18
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-3-1-17">
+            Netatalk 3.1.17
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-3-1-16">
+            Netatalk 3.1.16
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-3-1-15">
+            Netatalk 3.1.15
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-3-1-14">
+            Netatalk 3.1.14
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.13.html">
+            Netatalk 3.1.13
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.12.html">
+            Netatalk 3.1.12
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.11.html">
+            Netatalk 3.1.11
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.10.html">
+            Netatalk 3.1.10
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.9.html">
+            Netatalk 3.1.9
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.8.html">
+            Netatalk 3.1.8
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.7.html">
+            Netatalk 3.1.7
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.6.html">
+            Netatalk 3.1.6
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.5.html">
+            Netatalk 3.1.5
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.4.html">
+            Netatalk 3.1.4
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.3.html">
+            Netatalk 3.1.3
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.2.html">
+            Netatalk 3.1.2
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.1.html">
+            Netatalk 3.1.1
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes3.1.0.html">
+            Netatalk 3.1.0
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes-3.1-beta2.html">
+            Netatalk 3.1-beta2
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes-3.1-beta1.html">
+            Netatalk 3.1-beta1
+        </a>
+    </li>
+    <li>
+        <a href="3.1/ReleaseNotes-3.1-alpha1.html">
+            Netatalk 3.1-alpha1
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.8.html">
+            Netatalk 3.0.8
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.7.html">
+            Netatalk 3.0.7
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.6.html">
+            Netatalk 3.0.6
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.5.html">
+            Netatalk 3.0.5
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.4.html">
+            Netatalk 3.0.4
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.3.html">
+            Netatalk 3.0.3
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.2.html">
+            Netatalk 3.0.2
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes3.0.1.html">
+            Netatalk 3.0.1
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes-3.0.html">
+            Netatalk 3.0.0
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes-3.0-beta2.html">
+            Netatalk 3.0-beta2
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes-3.0-beta1.html">
+            Netatalk 3.0-beta1
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes-3.0-alpha3.html">
+            Netatalk 3.0-alpha3
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes-3.0-alpha2.html">
+            Netatalk 3.0-alpha2
+        </a>
+    </li>
+    <li>
+        <a href="3.0/ReleaseNotes-3.0-alpha1.html">
+            Netatalk 3.0-alpha1
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-2-3-0">
+            Netatalk 2.3.0
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-2-2-10">
+            Netatalk 2.2.10
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-2-2-9">
+            Netatalk 2.2.9
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-2-2-8">
+            Netatalk 2.2.8
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/Netatalk/netatalk/releases/tag/netatalk-2-2-7">
+            Netatalk 2.2.7
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2.6.html">
+            Netatalk 2.2.6
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2.5.html">
+            Netatalk 2.2.5
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2.4.html">
+            Netatalk 2.2.4
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2.3.html">
+            Netatalk 2.2.3
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2.1.html">
+            Netatalk 2.2.1
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2.0.html">
+            Netatalk 2.2.0
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2beta4.html">
+            Netatalk 2.2-beta4
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2beta3.html">
+            Netatalk 2.2-beta3
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2beta2.html">
+            Netatalk 2.2-beta2
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2beta1.html">
+            Netatalk 2.2-beta1
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2alpha5.html">
+            Netatalk 2.2-alpha5
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2alpha4.html">
+            Netatalk 2.2-alpha4
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2alpha2.html">
+            Netatalk 2.2-alpha2
+        </a>
+    </li>
+    <li>
+        <a href="2.2/ReleaseNotes2.2alpha1.html">
+            Netatalk 2.2-alpha1
+        </a>
+    </li>
+    <li>
+        <a href="2.1/ReleaseNotes2.1.6.html">
+            Netatalk 2.1.6
+        </a>
+    </li>
+    <li>
+        <a href="2.1/ReleaseNotes2.1.5.html">
+            Netatalk 2.1.5
+        </a>
+    </li>
+    <li>
+        <a href="2.1/ReleaseNotes2.1.4.html">
+            Netatalk 2.1.4
+        </a>
+    </li>
+    <li>
+        <a href="2.1/ReleaseNotes2.1.3.html">
+            Netatalk 2.1.3
+        </a>
+    </li>
+    <li>
+        <a href="2.1/ReleaseNotes2.1.2.html">
+            Netatalk 2.1.2
+        </a>
+    </li>
+    <li>
+        <a href="2.1/ReleaseNotes2.1.1.html">
+            Netatalk 2.1.1
+        </a>
+    </li>
+    <li>
+        <a href="2.1/ReleaseNotes2.1.html">
+            Netatalk 2.1.0
+        </a>
+    </li>
+    <li>
+        <a href="2.0/ReleaseNotes.html">
+            Netatalk 2.0.5
+        </a>
+    </li>
+    <li>
+        <a href="2.0/NEWS.html">
+            Older Netatalk Release Notes
+        </a>
+    </li>
+</ul>
+
+<?php include_once "footer.php"; ?>
