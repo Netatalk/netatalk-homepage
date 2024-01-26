fd551eab560b20c5eaa8669669d69741bbc2ba30
Daniel Markstedt <daniel@mindani.net>
2024-01-26 23:42:33 +0900

    remove deprecated script

--- upload.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Uploading web resources to sourceforge.net as $USER..."
-scp -r *.php css gfx docs 2.0 2.1 2.2 2.3 3.0 3.1 $USER@web.sourceforge.net:/home/project-web/netatalk/htdocs/
